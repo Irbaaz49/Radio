8b0bfae916f931313cc4984d87c277cc4c3d9220
Irbaaz49 <mwater196@gmail.com>
2021-12-26 20:54:49 +0530

    ready to push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Radio from './Radio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+  
+    <h1>The Radio </h1>
+    
+    <h2>Pick a genre, choose a station, start listenig </h2>
+ <Radio/>
+  </div>
+ 
   );
 }
 
